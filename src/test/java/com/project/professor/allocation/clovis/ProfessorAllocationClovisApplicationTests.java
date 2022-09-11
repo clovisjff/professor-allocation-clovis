5f4a0feea3a13fbfca15d42edfb342909d504ce1
Clóvis Ferreira <clovisjff@gmail.com>
2022-09-11 19:04:44 -0300

    Add Project

---
+++ src/test/java/com/project/professor/allocation/clovis/ProfessorAllocationClovisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.clovis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationClovisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
