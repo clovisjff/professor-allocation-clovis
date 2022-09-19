a8613db949f2c81fd4691283d20f4529ac7314de
Clóvis Ferreira <clovisjff@gmail.com>
2022-09-19 09:35:21 -0300

    Repository Layer - Allocation / Professor

--- src/main/java/com/project/professor/allocation/clovis/repository/AllocationRepository.java
+++ src/main/java/com/project/professor/allocation/clovis/repository/AllocationRepository.java
@@ -1,11 +1,21 @@
 package com.project.professor.allocation.clovis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.professor.allocation.clovis.entity.Allocation;
+import com.project.professor.allocation.clovis.entity.Professor;
 
 @Repository
 public interface AllocationRepository extends JpaRepository <Allocation, Long> {
+	
+	//SELECT * FROM  allocation WHERE course_id = 1?
+	List<Allocation> findByCourseId(Long courseId);
+	
+	//SELECT * FROM  allocation WHERE professor_id = 1?
+	List<Allocation> findByProfessorId(Long professorId);
+	
 
 }
--- src/main/java/com/project/professor/allocation/clovis/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/clovis/repository/ProfessorRepository.java
@@ -1,5 +1,8 @@
 package com.project.professor.allocation.clovis.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,18 @@ import com.project.professor.allocation.clovis.entity.Professor;
 
 @Repository
 public interface ProfessorRepository extends JpaRepository <Professor, Long> {
+	
+	//findByDepartmentId
+	//SELECT * FROM professor WHERE department_id =?1
+	List<Professor> findByDepartmentId(Long departmentId);
+	
+	//SELECT * FROM professor WHERE CPF=?1
+	//findByCpf
+	
+	Professor findByCpf(String cpf);
+	
+	//Optional<Professor> findByCpf(String cpf);
+	
+	
 
 }
