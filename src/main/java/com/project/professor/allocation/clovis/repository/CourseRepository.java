6d1cb6815cef5c9a0abf06377503bdf256fed528
Clóvis Ferreira <clovisjff@gmail.com>
2022-09-19 09:54:50 -0300

    Repository Layer - Update Course / Professor

--- src/main/java/com/project/professor/allocation/clovis/repository/CourseRepository.java
+++ src/main/java/com/project/professor/allocation/clovis/repository/CourseRepository.java
@@ -1,5 +1,7 @@
 package com.project.professor.allocation.clovis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.project.professor.allocation.clovis.entity.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository <Course, Long> {
+	
+	List<Course> findByNameContaining(String name);
 
 }
--- src/main/java/com/project/professor/allocation/clovis/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/clovis/repository/ProfessorRepository.java
@@ -9,19 +9,19 @@ import org.springframework.stereotype.Repository;
 import com.project.professor.allocation.clovis.entity.Professor;
 
 @Repository
-public interface ProfessorRepository extends JpaRepository <Professor, Long> {
-	
-	//findByDepartmentId
-	//SELECT * FROM professor WHERE department_id =?1
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+	// findByDepartmentId
+	// SELECT * FROM professor WHERE department_id =?1
 	List<Professor> findByDepartmentId(Long departmentId);
-	
-	//SELECT * FROM professor WHERE CPF=?1
-	//findByCpf
-	
+
+	// SELECT * FROM professor WHERE CPF=?1
+	// findByCpf
+
 	Professor findByCpf(String cpf);
-	
-	//Optional<Professor> findByCpf(String cpf);
-	
-	
+
+	// Optional<Professor> findByCpf(String cpf);
+
+	List<Professor> findByNameContaining(String name);
 
 }
